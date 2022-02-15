5e7b6350bde363b9f4b8a9056fbb104f2a29e33b
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-15 13:59:43 +0200

    js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -11,64 +11,24 @@ const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
     
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);   
-    galleryContainer.addEventListener('click', onGalleryContainerClick); 
+    
 function createGalleryMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
             return `
                 <div class="gallery__item">
                     <a class="gallery__link" href="${original}">
-                        <img
-                            class="gallery__image"
-                            src="${preview}"
-                            data-source="${original}"
-                            alt="${description}"
-                        />
-                    </a>
-                </div>`;
+                            <img class="gallery__image" 
+                                src="${preview}" 
+                                alt="${description}"
+                            />
+                        </a>
+                </div >
+                `;
             })
         .join('');
 }
 
-
-function onGalleryContainerClick(event) {
-    event.preventDefault();
-
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    } 
-    
-  modalShow(event.target.dataset.source);
-}
-
-let instance;
-function modalShow(src) {
-  instance = basicLightbox.create( `<img src="${src}"></img>`,
-    {
-      onShow: instance => {
-        addListener();
-      },
-      onClose: instance => {
-        removeListener();
-      },
-    },
-  );
-  instance.show();
-}
-
-function addListener() {
-  window.addEventListener('keydown', onEscClick);
-}
-
-function onEscClick(event) {
-  if (event.code === 'Escape') {
-    instance.close();
-  }
-}
-
-function removeListener() {
-  window.removeEventListener('keydown', onEscClick);
-}
           
 let modalGallery = new SimpleLightbox('.gallery a', {
     caption: true,
