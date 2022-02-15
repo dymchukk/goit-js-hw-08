00a073276b0994dacaf1c8948f9bbfb48e4b183f
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-15 13:03:40 +0200

    js change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,35 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);   
+    
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                            <img class="gallery__image" 
+                                src="${preview}" 
+                                alt="${description}"
+                            />
+                        </a>
+                </div >
+                `;
+            })
+        .join('');
+}
+
+          
+let modalGallery = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,    
+}); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form-state';
+
+const input = {};
+
+const inputForm = document.querySelector('.feedback-form');
+ 
+inputForm.addEventListener('input', throttle(newInput, 500));   
+    
+function newInput(event) {
+    input.email = inputForm.elements.email.value;
+    input.message = inputForm.elements.message.value;
+    localStorage.setItem(KEY, JSON.stringify(input));
+}
+ 
+updatePage();
+
+function updatePage() {
+    
+    const savedData = localStorage.getItem(KEY);
+
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        inputForm.email.value = email;
+        inputForm.message.value = message;
+    }
+}
+
+inputForm.addEventListener('submit', onFormSubmit);  
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+    console.log({
+        email: inputForm.email.value,
+        message: inputForm.message.value
+    });
+
+    event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+}
