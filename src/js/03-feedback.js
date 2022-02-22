0e87eed1373e72a2da7a609f86e80df3c8167ab0
Dymchuk Ekateryna <dymchukkat@gmail.com>
2022-02-22 15:37:25 +0200

    js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -19,54 +19,6 @@
 Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.*/
 
 
-
-/*
-import throttle from 'lodash.throttle';
-
-const KEY = 'feedback-form-state';
-
-const input = {};
-
-const inputForm = document.querySelector('.feedback-form');
- 
-inputForm.addEventListener('input', throttle(newInput, 500));   
- 
-function newInput(event) {
-  
-    input.email = inputForm.elements.email.value;
-    input.message = inputForm.elements.message.value;
-    localStorage.setItem(KEY, JSON.stringify(input));
-
-}
- 
-updatePage();
-
-function updatePage() {
-    
-    const savedData = localStorage.getItem(KEY);
-
-    if (savedData) {
-        const { email, message } = JSON.parse(savedData);
-        inputForm.email.value = email;
-        inputForm.message.value = message;
-    }
-  return
-}
-
-inputForm.addEventListener('submit', onFormSubmit);  
-
-function onFormSubmit(event) {
-  event.preventDefault();
- 
-if(!inputForm.email.value || !inputForm.message.value)return;
-    event.currentTarget.reset();
-  localStorage.removeItem(KEY);
-  console.log(input)
-}
-*/
-
-
-
 import throttle from 'lodash.throttle';
 
 const KEY = 'feedback-form-state';
